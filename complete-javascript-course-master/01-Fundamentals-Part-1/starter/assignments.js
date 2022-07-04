d8f217ac9c16139a3a0849e514248145cde4a1e2
Chris Gobal <ch.gobal@gmail.com>
2022-07-04 16:25:51 -0700

    1 JS file modified++

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,7 @@
+let country = "Armenia";
+let population = 2963000;
+let continent = "Europe";
+
+console.log(country);
+console.log(population);
+console.log(continent );
--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -9,14 +9,22 @@ console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
-let years = 3;
-let _new = 23;
+// Variable name conventions
+let jonas_matilda = "JM";
+let $function = 27;
 
-let Person = "jonas"; // Recommended By convention - Do not start variable name with upper case letter — but it is still legal (we can do it this way)
+let person = "jonas";
 let PI = 3.1415;
+let Person = "jonas"; // Recommended By convention - Do not start variable name with upper case letter — but it is still legal (we can do it this way)
 
-let myFirstJob = "Programmer";
+let myFirstJob = "Coder";
 let myCurrentJob = "Teacher";
 
-let job1 = "Programmer";
-let job2 = "Teacher";
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+
+
+
+
