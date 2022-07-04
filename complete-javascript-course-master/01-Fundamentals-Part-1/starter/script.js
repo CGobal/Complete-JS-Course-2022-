ff24b619aa07776caa76f4d0c0ee80c2f13f76de
Chris Gobal <ch.gobal@gmail.com>
2022-07-04 15:26:31 -0700

    1 JS file modified +

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,22 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript Is FUN!!!");
-
-40 + 8 + 23 - 10
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+
+let firstName = "Matilda";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let years = 3;
+let _new = 23;
+
+let Person = "jonas"; // Recommended By convention - Do not start variable name with upper case letter — but it is still legal (we can do it this way)
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "Programmer";
+let job2 = "Teacher";
