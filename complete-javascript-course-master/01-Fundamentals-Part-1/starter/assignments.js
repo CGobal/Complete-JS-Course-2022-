80efbf304b8f4ed5f40bb13fea21f406aaabea1f
Chris Gobal <ch.gobal@gmail.com>
2022-07-04 21:17:15 -0700

    1 JS file modified++

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -1,20 +1,19 @@
 
 //LECTURE: Values and Variables
-const country = "Armenia";
-let population = 2963000;
+const myCountry = "Armenia";
+let population = 2974282;
 const continent = "Europe";
 
 // console.log(country);
 // console.log(population);
 // console.log(continent );
 
-
 //LECTURE: Data Types
 
 const isIsland = false;
 let language;
 console.log("**********************");
-console.log(typeof country);
+console.log(typeof myCountry);
 console.log(typeof population);
 console.log(typeof isIsland);
 console.log(typeof language);
@@ -23,3 +22,20 @@ console.log(typeof language);
 
 language = "Armenian";
 //isIsland = true; // => Uncaught TypeError: Assignment to constant variable.
+
+// LECTURE: Basic Operators
+
+console.log("**********************");
+
+let halfPopulation = population / 2;
+//console.log(halfPopulation);
+population++;
+console.log(population);
+let populationFinland = 6000000;
+console.log(population > populationFinland);
+let populationAverage = 33000000;
+console.log(population < populationAverage);
+let populationInMillion = population / 1000000;
+const description = myCountry + " is in " + continent + ", and its " + populationInMillion.toFixed(2) + " million people speak " + language;
+console.log(description);
+
