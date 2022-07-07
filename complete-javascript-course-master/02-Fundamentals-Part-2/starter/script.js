03e1eae89c9b286b85b7b9d60516d8558082597d
Chris Gobal <ch.gobal@gmail.com>
2022-07-06 17:50:53 -0700

    3-dr Day JS Fundamentals Part 1-FINISH

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -11,7 +11,7 @@ if(hasDriversLicense) console.log('I can drive :D');
 // const private = 534;
 // const if = 55;
 
-*/
+
 
 // Functions
 
@@ -24,14 +24,29 @@ logger();
 logger();
 logger();
 
+*/
 // While constructing a function we can also specify something called PARAMETERS and parameters are like variables that are specific only to this function and they will get defined once we call the function.
 
 function fruitProcessor(apples, oranges) {
-    console.log(apples, oranges);
     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
     return juice;
 }
 
-const appleJuice = fruitProcessor(5, 0)   // This actual values (5/0) are called ARGUMENTS
+const appleJuice = fruitProcessor(5, 0);   // This actual values (5/0) are called ARGUMENTS
+// capture the value returned from the function execution into a variable "appleJuice"
 
 console.log(appleJuice);
+//console.log(fruitProcessor(5, 0));
+/*
+We are defining specific values here which are the arguments (5, 0)
+which will get passed into the function as these parameters (apples, oranges). 
+And so when the function is running now, apples in here will be five and oranges will be zero.
+Then we use these values to build this juice string here then we return that value from the function and what that means is, that basically the result of calling this function here will be the juice value that was just returned
+*/
+
+const appleOrangeJuice = fruitProcessor(2, 4); 
+console.log(appleOrangeJuice);
+
+// Keep your code DRY - don't repeate yourself .  Functions are perfect for implementing dry code because they are reusable code blocks, that together, make up all applications.
+
+const num = Number('23');
