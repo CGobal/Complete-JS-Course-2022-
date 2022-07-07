2c266575116b7ab8c5f69899d1a000e3cd21761b
Chris Gobal <ch.gobal@gmail.com>
2022-07-07 11:58:28 -0700

    Day 4 - JS GFundamentals - Part 2

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -51,7 +51,7 @@ console.log(appleOrangeJuice);
 
 const num = Number('23');
 
-*/
+
 
 // Function Declarations vs. Expressions
 
@@ -91,3 +91,22 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 
 console.log(yearsUntilRetirement(1991, "Jonas"));
 console.log(yearsUntilRetirement(1980, "Bob"));
+
+*/
+
+
+//Functions Calling Other Functions
+
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));
