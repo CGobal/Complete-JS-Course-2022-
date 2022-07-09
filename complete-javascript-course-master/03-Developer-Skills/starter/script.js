7af6d2187459a0a259406c046ed3c9ad93dc7669
Chris Gobal <ch.gobal@gmail.com>
2022-07-08 21:00:37 -0700

    fff

--- complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -1,3 +1,12 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const xyz = 23;
+if (xyz === 23) console.log(23);
+
+const calAge = birtYear => 2037 - birtYear;
+
+console.log(xyz);
+
+// BUG TODO
+console.log('55555555');
